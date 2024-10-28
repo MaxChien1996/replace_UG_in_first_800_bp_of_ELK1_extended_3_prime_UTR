a49d08105c23f5a1c6d422390b4b11a3876054f1
Max_Chien <MaxC.ZaoYouJean@gmail.com>
2024-10-28 16:35:48 +0000

    final check at least this what I think

--- 5_remove UG in upstream ELK1 3 prime UTR/5.1_main.R
+++ 5_remove UG in upstream ELK1 3 prime UTR/5.1_main.R
@@ -2,9 +2,8 @@ library(tidyverse)
 
 
 
-# oPools_1 only contains upstream ELK1 3'-UTR
-
-# wild type upstream ELK1 3'-UTR is the oPools_1 in the "../1_generate candidate sequences/oPool_regions.py"
+# oPools_1 only contains upstream 192 bp of cryptic ELK1 3'-UTR
+# the sequence of oPools_1 is in the "../1_generate candidate sequences/oPool_regions.py"
 wild_type <- "CAAAAGACTCCTGGGCTCACCTGTTAGCGCTGGCCCAGCCCAGGCCTTGGGACCTGGGGGTTGGTGATTTGGGGGACAGTGCTACACTCGTCTCCACTGTTTGTTTTACTTCCCCAAAATGGACCTTTTTTTTTTCTAAAGAGTCCCAGAGAATGGGGAATTGTTCCTGTAAATATATATTTTTCAAAGTGA"
 
 # sequence from "../1_generate candidate sequences/1.2_UG_removed_upstream_ELK1_3_prime_UTR.ipynb"
@@ -20,10 +19,10 @@ oPools_1 <- data_frame(
 
 
 
-# extra UG version from:
-# ~OneDrive-UniversityCollegeLondon/Lab note
-# /PhD/2024.06.17_Put a 15-mer barcode right after mGreenLantern and start removing UG in ELK1 extended 3 prime-UTR
-# /manually add more UG.dna
+# extra UG version of "the first 350 bp of ELK1 cryptic 3'-UTR" is from:
+  # ~/The Francis Crick Dropbox/ZaoYou Jean/Lab note
+  # /PhD/2024.06.17_Put a 15-mer barcode right after mGreenLantern and start removing UG in ELK1 extended 3 prime-UTR
+  # /manually add more UG.dna
 extra_UG_oPools_2 <- "TCCTGTAAATATATATTTTTCAAAGTGATGCTGTGGGCTGCTGGTGTGTTTTGTGTGATGTTGTGTGTGTGTGCTGTGCGTGGCTGGCATGTGTGTGTGTGTGGGGTGTGTGGCTGTGTGCTGTTGTGTGTGAATGGGTGTGATGGCGTGTGTGTGAAACAATAGTAGGGACTGCATATCGGTA"
 extra_UG_oPools_3 <- "AAACAATAGTAGGGACTGCATATCGGTATGTGTGTGTGTGTGTGTGTGGTGTGATGCTGTGATTGTGTGTGTGTGTGTGTGTGTGTTGTGTGTGTGTGTGTTTGATGGTGTGTGTATGTGGTGAATTGTGGCTGTGTTTGTGAAAATAACTGTGCCTGTGTGAATTGTGATGTGTGTGTTGAATGTGCTGTGATGTGATGTGTGAAATAATGTGTACGAGAAGGGAACGGGGACTGCAGCCCT"
 
@@ -33,9 +32,12 @@ Gibson_assembly_overlapping_oPools_2_3 = "AAACAATAGTAGGGACTGCATATCGGTA"
 extra_UG_whole_sequence <- paste0(str_remove(wild_type, Gibson_assembly_overlapping_oPools_1_2), 
                                   str_remove(extra_UG_oPools_2, Gibson_assembly_overlapping_oPools_2_3), 
                                   extra_UG_oPools_3)
+# whole sequence means oPools_1 + oPools_2 + oPools_3
+
+
 
 selected_sequences <- read_csv("../4_pick sequences/selected_sequences.csv") %>% 
-  # add the extra UG sequence
+  # add the extra UG version of sequence
   rbind(list(portion = "200%", 
              replicate = "", 
              portions = "", 
@@ -58,7 +60,7 @@ oPools_3 <- selected_sequences %>%
   mutate(`Pool name` = "ELK1_oPools_3") %>% 
   mutate(sequence_name = paste0(portion, "_", replicate)) %>% 
   mutate(sequence_name = ifelse(sequence_name == "200%_", "200%_#1", sequence_name)) %>% 
-  # because 10%___#1 is the same as 25%___#1
+  # because 10%___#1 is as same as 25%___#1
   mutate(sequence_name = ifelse(sequence_name == "10%_#1", "10%_25%_#1", sequence_name)) %>% 
   mutate(sequence_name = ifelse(sequence_name == "25%_#1", "10%_25%_#1", sequence_name)) %>%
   select(`Pool name`, oPools_3_sequence, sequence_name) %>% 
